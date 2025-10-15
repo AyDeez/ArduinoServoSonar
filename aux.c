a52ecc18404c93ce9e7caaa18f438fc7019aa401
AyDeez <alex.dinenno@gmail.com>
2025-10-15 16:51:51 +0200

    implemented UART communication

--- aux.c
+++ aux.c
@@ -14,3 +14,42 @@ void UART_init() {
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); //8-bit data
     UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); //enables RX-TX
 }
+
+// waits for transmission completed, looping on status bit, then starts transmission
+void UART_putChar(uint8_t c) {
+    while (!(UCSR0A & (1 << UDRE0)) );
+    UDR0 = c;
+}
+
+// sends a passed string
+void UART_putString(uint8_t* buf) {
+    while (*buf) {
+        UART_putChar(*buf);
+        buf++;
+    }
+}
+
+// waits for incoming data, looping on status bit, and returns the received data
+uint8_t UART_getChar() {
+    while (!(UCSR0A & (1 << RXC0)));
+    return UDR0;
+}
+
+// reads a string until the first \n and saves it in the buffer, returns the size read
+uint8_t UART_getString(uint8_t* buf) {
+    uint8_t* b0 = buf; //beginning of buffer
+    while(1){
+        uint8_t c = UART_getChar();
+        *buf=c;
+        buf++;
+        // reading a 0 terminates the string
+        if (c==0) return buf-b0;
+        // reading a \n or a \r return results
+        // in forcedly terminating the string
+        if(c=='\n'||c=='\r'){
+            *buf=0;
+            buf++;
+            return buf-b0;
+        }
+    }
+}
--- aux.h
+++ aux.h
@@ -6,13 +6,13 @@
 // --------------------------------- //
 
 #include <avr/io.h>
-#include <util/delay.h>
 
 #ifndef AUX_H
 #define AUX_H
 
 // uart things
 #define BAUD 19200
+#define F_CPU 16000000UL
 #define MYUBRR (F_CPU/16/BAUD-1)
 #define MAX_BUFF 256
 
@@ -25,12 +25,20 @@
 #define SENSOR_DDR DDRB
 #define SENSOR_PORT PORTB
 
-// servo on pin 13
-#define SERVO_BIT 7
+// servo on pin 10
+#define SERVO_BIT 4
 #define SERVO_DDR DDRB
 #define SERVO_PORT PORTB
 
+// servo
+#define DEFAULT_MIN_ANGLE 0
+#define DEFAULT_MAX_ANGLE 180
+
 // function declarations
 void UART_init();
+void UART_putChar(uint8_t c);
+void UART_putString(uint8_t* buf);
+uint8_t UART_getChar();
+uint8_t UART_getString(uint8_t* buf);
 
 #endif
